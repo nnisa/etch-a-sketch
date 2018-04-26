20c16783a22db08b8d71efa8416695a10daf9d61
Noshin Anjum Nisa <nisanoshin@gmail.com>
2018-04-26 02:28:39 -0400

    WORKS

--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -1,41 +1,38 @@
 
 #define PINX A0 // x axis
 #define PINY A1 // y axis
-#define BUTTON 2 // reset pin
-#define RANDOM 6 // random width pin
-#define RANDOMC 12 // random color pin
+#define BUTTON 12 // reset pin
+#define WIDTH 7 // random width pin
+#define COLOR 2 // random color pin
 
 unsigned long targetTime=0;
 const unsigned long interval=500; 
 
 void setup(){
-  Serial.begin(115200);
+//  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(PINX, INPUT);
   pinMode(PINY, INPUT);
   pinMode(BUTTON, INPUT);
-  pinMode(RANDOM, INPUT);
-  pinMode(RANDOMC, INPUT);
+  pinMode(WIDTH, INPUT);
+  pinMode(COLOR, INPUT);
 }
 
 void loop(){
   if(digitalRead(BUTTON)) {
     Serial.println("rst");
   }
-  if(digitalRead(RANDOM)) {
-    Serial.println("random");
+  if(digitalRead(WIDTH)) {
+    Serial.println("width");
   }
-  if(digitalRead(RANDOMC)) {
-    Serial.println("randomc");
+  if(digitalRead(COLOR)) {
+    Serial.println("color");
   }
   if(millis()>=targetTime){
-		targetTime= millis()+interval;
-		Serial.println(String(analogRead(PINX))+","+String(analogRead(PINY)));
-	}
+    targetTime= millis()+interval;
+    Serial.println(String(analogRead(PINX))+","+String(analogRead(PINY)));
+  }
 }
-
-
-
-
 
 
 // #define SENSORPINA A0 // x axis
